96066906ccccc80e5899eb45d57618f50bd38d0d
Guillermo Ampie <ga.ampie@gmail.com>
2021-06-11 13:51:46 -0500

    Add a log messages when the "/" endpoint is reached

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  console.log(`<< / >> endpoint reached`)
+  res.send('Hello World!')
+})
 
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000!`)
